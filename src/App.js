c9075d0e6799340233f23fe7445b3f56945adf29
wonbeenna <nwbnwb@naver.com>
2022-12-10 22:20:55 +0900

    count practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  const handlePlus = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
+  const handleMinus = () => {
+    setCount((prevState) => prevState - 1);
+  };
+
+  const handleDisabled = () => {
+    setDisabled((prevState) => !prevState);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h3 data-testid="counter">{count}</h3>
+        <div>
+          <button data-testid="minus-button" onClick={handleMinus} disabled={disabled}>
+            -
+          </button>
+          <button data-testid="plus-button" onClick={handlePlus} disabled={disabled}>
+            +
+          </button>
+        </div>
+        <div>
+          <button data-testid="on/off-button" style={{backgroundColor: 'blue'}} onClick={handleDisabled}>
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,58 @@
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object로 원하는 엘리멘트에 접근(ID)
+  const counterElement = screen.getByTestId('counter');
+  // id가 counter인 엘리멘트의 텍스트가 0인가?
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test('Minus Button', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('minus-button');
+  expect(buttonElement).toHaveTextContent('-');
+});
+
+test('Plus Button', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('plus-button');
+  expect(buttonElement).toHaveTextContent('+');
+});
+
+test('When the + button is pressed, the counter change to 1', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('plus-button');
+  // 버튼 클릭
+  fireEvent.click(buttonElement);
+  // plus 버튼 클릭시 counter 엘리먼트는 1
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test('When the - button is pressed, the counter change to -1', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('minus-button');
+  // 버튼 클릭
+  fireEvent.click(buttonElement);
+  // minus 버튼 클릭시 counter 엘리먼트는 -1
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(-1);
+});
+
+test('on/off button has blue color', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('on/off-button');
+  expect(buttonElement).toHaveStyle({backgroundColor: 'blue'});
+});
+
+test('Prevent -,+ button from being pressed when the on/off button is clicked', () => {
+  render(<App />);
+  const onOffButtonElement = screen.getByTestId('on/off-button');
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId('plus-button');
+  const minusButtonElement = screen.getByTestId('minus-button');
+  expect(plusButtonElement).toBeDisabled();
+  expect(minusButtonElement).toBeDisabled();
 });
