56e766da606979fa37478afe377831d4038ed148
wonbeenna <nwbnwb@naver.com>
2022-12-28 01:09:13 +0900

    msw, waitFor

---
+++ sundaes-on-demand/src/pages/common/AlertBanner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Alert from 'react-bootstrap/Alert';
+
+const AlertBanner = ({message, variant}) => {
+  const alertMessage = message || 'An unexpected error occurred. Please try again later.';
+  const alertVariant = variant || 'danger';
+
+  return (
+    <Alert role="alert" variant={alertVariant} style={{backgroundColor: 'red'}}>
+      {alertMessage}
+    </Alert>
+  );
+};
+
+export default AlertBanner;
---
+++ sundaes-on-demand/src/pages/entry/test/Options.test.jsx
@@ -0,0 +1,30 @@
+import {render, screen} from '@testing-library/react';
+import Options from '../Options';
+
+test('displays image for each scoop option from server', async () => {
+  render(<Options optionType="scoops" />);
+
+  // find images
+  const scoopImages = await screen.findAllByRole('img', {name: /scoop$/i});
+  expect(scoopImages).toHaveLength(2);
+
+  // alt text of images
+  const altText = scoopImages.map((el) => el.alt);
+
+  // 배열과 객체는 toEqual
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays image for each toppings option from server', async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images
+  const toppingImages = await screen.findAllByRole('img', {name: /topping$/i});
+  expect(toppingImages).toHaveLength(3);
+
+  // alt text of images
+  const altText = toppingImages.map((el) => el.alt);
+
+  // 배열과 객체는 toEqual
+  expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping']);
+});
